14f92f8b5b9c35380490c5bb6e00029cb22fd86c
Srihari Balgam <sriharibalgam4@gmail.com>
2023-03-28 10:43:08 -0400

    Update dbConnection.config.ts

--- app/config/dbConnection.config.ts
+++ app/config/dbConnection.config.ts
@@ -1,26 +1,22 @@
 const host = process.env.MONGO_HOST || 'localhost';
-const mongoport = process.env.MONGO_PORT || 27017;
+const mongoPort = process.env.MONGO_PORT || 27017;
 const database = process.env.MONGO_NAME || 'eruditekids';
-const password = process.env.MONGO_PASSWORD || 'eruditekids';
-const username = process.env.MONGO_USER || 'eruditekids';
+const password = process.env.MONGO_PASSWORD || '';
+const username = process.env.MONGO_USER || '';
 const ssl = process.env.MONGO_SSL || false;
 const credentials = username ? `${username}:${encodeURIComponent(password)}@` : '';
-const poolSize = process.env.MONGO_POOL_SIZE ? parseInt(process.env.MONGO_POOL_SIZE, 10) : 100;
 const environment = process.env.ENV_NAME || 'local';
 
 // mongodb+srv://eruditekids:eruditekids@eruditekids.6mwrn.mongodb.net/
 const serverDBUrl = `mongodb+srv://${credentials}${host}/${database}?retryWrites=true`;
-const localDBUrl = `mongodb://${credentials}${host}:${mongoport}/${database}?ssl=${ssl}`;
+const localDBUrl = `mongodb://${credentials}${host}:${mongoPort}/${database}?ssl=${ssl}`;
 
 const dbConnection = {
   env: environment,
   host,
   dbUrl: (process.env.ENV_NAME || 'LOCAL') !== 'LOCAL' ? serverDBUrl : localDBUrl,
   options: {
-    poolSize,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
+    autoIndex: true
   }
 };
 
