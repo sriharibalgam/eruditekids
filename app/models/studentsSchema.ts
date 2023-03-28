181bcd760c62cb59a5524f1a3ca58557e11178a9
Srihari Balgam <sriharibalgam4@gmail.com>
2023-03-28 10:46:17 -0400

    Update studentsSchema.ts

--- app/models/studentsSchema.ts
+++ app/models/studentsSchema.ts
@@ -1,14 +1,19 @@
 import mongoose, { Schema } from 'mongoose';
-import { IStudent } from '../interfaces/stundents.interface';
+import { IStudent } from '../interfaces/students.interface';
 
 const studentSchema: Schema = new Schema({
     firstName: { type: String },
     lastName: { type: String },
     email: { type: String, unique: true, required: true },
     password: { type: String },
-    role: { type: String }
+    role: { type: String },
+    subscriptions: { type: Object },
+    status: { type: Boolean }
+}, {
+    strict: false,
+    collection: 'students'
 });
 
-const StudentsModel = mongoose.model< IStudent>('Students', studentSchema);
+const StudentsModel = mongoose.model< IStudent>('students', studentSchema);
 
 export default StudentsModel;
